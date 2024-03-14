27c037d1ce0f4756c8f52f4e8b2ee5aee044d644
Eliili <101203108+Eliili@users.noreply.github.com>
2024-03-14 14:06:37 +0200

    First

---
+++ Assets/Scripts/PatrolBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolBehaviour : MonoBehaviour
+{
+    public Stats enemyStats;
+    
+    public Transform[] patrolPoints;
+    
+    public int currentPatrolPoint = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public struct Stats
+    {
+        [Header("Enemy Settings")]
+        [Tooltip("How fast the enemy walks (only when idle is true).")]
+        public float walkSpeed;
+
+        [Tooltip("How fast the enemy turns in circles as they're walking (only when idle is true).")]
+        public float rotateSpeed;
+
+        [Tooltip("How fast the enemy runs after the player (only when idle is false).")]
+        public float chaseSpeed;
+
+        [Tooltip("Whether the enemy is idle or not. Once the player is within distance, idle will turn false and the enemy will chase the player.")]
+        public bool idle;
+
+        [Tooltip("How close the enemy needs to be to explode")]
+        public float explodeDist;
+
+    }
+    private void Patrol()
+    {
+        Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
+        transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStats.walkSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
+        {
+            currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Length;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
